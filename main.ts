c1da486bc69749e1baa7da91759aaabcd0dd7ea9
IsobelPaes <95632385+IsobelPaes@users.noreply.github.com>
2021-12-06 14:24:43 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    basic.showIcon(IconNames.Heart)
+    textLine = serial.readLine().split(":")
+    radio.sendValue(textLine[0], parseFloat(textLine[1]))
+    basic.pause(1000)
+    basic.showIcon(IconNames.SmallHeart)
+})
+let textLine: string[] = []
+radio.setGroup(99)
+basic.showIcon(IconNames.Ghost)
